a8cba1ab6d8a7bdcfd8a648891f97fed7ceaf24c
Nishant Shah <nshah27@binghamton.edu>
2017-10-16 19:22:53 +0000

    ModerateRisk.java updated with setting a flag for printing results.

--- airportSecurityState/src/airportSecurityState/airportStates/ModerateRisk.java
+++ airportSecurityState/src/airportSecurityState/airportStates/ModerateRisk.java
@@ -9,16 +9,22 @@ public class ModerateRisk implements AirportStateI {
 	}
 	
 	public void tightenOrLoosenSecurity() {
+		
+		System.out.println("currently in MOD");
+		System.out.println(sf.getAvgTraffic());
+		System.out.println(sf.getAvgProhibited());
 		if((sf.getAvgTraffic() >= 8) || (sf.getAvgProhibited() >= 2)) {
 			sf.setAsi(sf.getHighRisk());
+			sf.setFlag(2);
 			
-			System.out.println("HIGH");
+			System.out.println("change to HIGH");
 		}
 		
 		else if((sf.getAvgTraffic() >= 0 && sf.getAvgTraffic() < 4) && (sf.getAvgProhibited() >= 0 && sf.getAvgProhibited() < 1)) {
 			sf.setAsi(sf.getLowRisk());
+			sf.setFlag(0);
 			
-			System.out.println("LOW");
+			System.out.println("change to LOW");
 		}
 	}
 }
