1fc217ac2af002f7dbdfebfdc72d1a33deef688c
Nishant Shah <nshah27@binghamton.edu>
2017-10-16 19:21:28 +0000

    LowRisk.java updated with setting flag to print appropriate results. Yet to implement Logger

--- airportSecurityState/src/airportSecurityState/airportStates/LowRisk.java
+++ airportSecurityState/src/airportSecurityState/airportStates/LowRisk.java
@@ -9,10 +9,14 @@ public class LowRisk implements AirportStateI {
 	}
 	
 	public void tightenOrLoosenSecurity() {
+		
+		System.out.println("currently in LOW");
+		System.out.println(sf.getAvgTraffic());
+		System.out.println(sf.getAvgProhibited());
 		if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) || (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
 			sf.setAsi(sf.getModerateRisk());
-			
-			System.out.println("MOD");
+			sf.setFlag(1);
+			System.out.println("change to MOD");
 		}
 	}
 }
