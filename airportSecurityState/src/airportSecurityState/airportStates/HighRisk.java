a225b5d4c6966d944d1941a3ad2c41a2b22c2d7f
Nishant Shah <nshah27@binghamton.edu>
2017-10-16 19:20:05 +0000

    added proper conditions to switch back to Moderate Risk from High Risk. Yet to implement Logger

--- airportSecurityState/src/airportSecurityState/airportStates/HighRisk.java
+++ airportSecurityState/src/airportSecurityState/airportStates/HighRisk.java
@@ -9,10 +9,32 @@ public class HighRisk implements AirportStateI {
 	}
 	
 	public void tightenOrLoosenSecurity() {
+		
+		System.out.println("currently in HIGH");
+		System.out.println(sf.getAvgTraffic());
+		System.out.println(sf.getAvgProhibited());
+		
 		if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) && (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
 			sf.setAsi(sf.getModerateRisk());
+			sf.setFlag(1);
 			
-			System.out.println("MOD");
+			System.out.println("change to MOD");
 		}
+		
+		else if((sf.getAvgTraffic() >= 0 && sf.getAvgTraffic() < 4) && (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
+			sf.setAsi(sf.getModerateRisk());
+			sf.setFlag(1);
+			
+			System.out.println("change to MOD");
+		}
+		
+		else if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) && (sf.getAvgProhibited() >= 0 && sf.getAvgProhibited() < 1)) {
+			sf.setAsi(sf.getModerateRisk());
+			sf.setFlag(1);
+			
+			System.out.println("change to MOD");
+		}
+		
+		
 	}
 }
