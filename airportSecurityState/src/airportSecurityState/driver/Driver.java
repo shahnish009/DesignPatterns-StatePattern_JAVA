cfb79fb95757a90209fbca467db7695de2edd842
Nishant Shah <nshah27@binghamton.edu>
2017-10-18 01:58:47 +0000

    updated structure with using MyLogger and little design upgrades

--- airportSecurityState/src/airportSecurityState/driver/Driver.java
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -18,40 +18,30 @@ public class Driver {
 		nailcutter,
 		blade,
 		knife
-	}
-	
-	private String opRes(SecurityFactors sf, ArrayList<Integer> al) {
-		if(0 == sf.getFlag()) {
-			return al.get(0) + " " + al.get(2) + " " + al.get(4) + " " + al.get(6) + " " + al.get(8); 
-		}
-		else if(1 == sf.getFlag()) {
-			return al.get(1) + " " + al.get(2) + " " + al.get(4) + " " + al.get(7) + " " + al.get(8); 
-		}
-		else if(2 == sf.getFlag()) {
-			return al.get(1) + " " + al.get(3) + " " + al.get(5) + " " + al.get(7) + " " + al.get(9); 
-		}
-		
-		return null;
-	}
+	};
 
 	public static void main(String[] args) {
 
 		String ipFile = null;
 		String opFile = null;
 		String ip = null;
+		
 		double travellers = 0;
 		double days = 0;
 		double prohibited = 0;
+		
 		int debugValueSetter = -1;
+		
 		String[] info = null;
 		String[] values = null;
+		
 		FileProcessor fileP = null;
-		ArrayList<Integer> opID = null;
 		
 		try {
-			if(args.length == 3) {
+			if(3 == args.length) {
        	        ipFile = args[0];
 	            opFile = args[1];
+	            
 	            debugValueSetter = Integer.parseInt(args[2]);
 	            if(!(debugValueSetter >= 0 && debugValueSetter <= 4)) {
 	            	throw new RuntimeException("Debug value must be integers between 0 and 4 inclusive");
@@ -72,14 +62,9 @@ public class Driver {
 			fileP = new FileProcessor(ipFile);
 		}
 		catch(FileNotFoundException e) {
-			System.err.println(e + " input file not found.");
+			System.err.println(" input file not found.");
 			e.printStackTrace();
 			System.exit(1);
-		}
-		
-		opID = new ArrayList<Integer>();
-		for(int i = 1; i <= 10; i++) {
-			opID.add(i);
 		}
 	
 		SecurityFactors secFac = new SecurityFactors(0,0);
@@ -93,33 +78,68 @@ public class Driver {
 				values = info[0].split(":");
 				days = Double.parseDouble(values[1]);
 				values = null;
+
 				values = info[3].split(":");
-				for(prohibitedItems pi : prohibitedItems.values()) {
-					if(pi.name().equalsIgnoreCase(values[1])) {
-						prohibited+=1;
-					}
+				
+				if(d.checkInEnum(values[1])) {
+					prohibited+=1;
 				}
+				
 				travellers+=1;
-				System.out.println("Day: " + days + "Travellers: " + travellers);
+				
 				secFac.setAvgTraffic(travellers / days);
 				secFac.setAvgProhibited(prohibited / days);
 				secFac.tightenOrLoosenSecurity();
-				r.storeNewResult(d.opRes(secFac, opID));
+				
+				r.storeNewResult(d.opRes(secFac));
+				
+				MyLogger.writeMessage(d.opRes(secFac) + " added to Results", MyLogger.DebugLevel.IN_RESULTS);
 			}
 			catch(Exception e) {
-				System.err.println(e + "\nInput file nor formatted properly");
-				e.printStackTrace();
-				System.exit(1);
-			}
-		
-			try {
-				r.writeToFile();
-			}
-			catch(FileNotFoundException e) {
-				System.err.println("Output file cannot be generated");
+				System.err.println("Input file nor formatted properly");
 				e.printStackTrace();
 				System.exit(1);
 			}
 		}
+		
+		try {
+			r.writeToFile();
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("Output file cannot be generated");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	private boolean checkInEnum(String s) {
+		
+		boolean retVal = false;
+		for(prohibitedItems pi : prohibitedItems.values()) {
+			if(pi.name().equalsIgnoreCase(s)) {
+				retVal = true;
+			}
+		}
+		return retVal;
+	}
+	
+	private String opRes(SecurityFactors sf) {
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for(int i = 1; i <= 10; i++) {
+			al.add(i);
+		}
+		
+		if(0 == sf.getFlag()) {
+			return al.get(0) + " " + al.get(2) + " " + al.get(4) + " " + al.get(6) + " " + al.get(8); 
+		}
+		else if(1 == sf.getFlag()) {
+			return al.get(1) + " " + al.get(2) + " " + al.get(4) + " " + al.get(7) + " " + al.get(8); 
+		}
+		else if(2 == sf.getFlag()) {
+			return al.get(1) + " " + al.get(3) + " " + al.get(5) + " " + al.get(7) + " " + al.get(9); 
+		}
+		
+		return null;
 	}
 }
