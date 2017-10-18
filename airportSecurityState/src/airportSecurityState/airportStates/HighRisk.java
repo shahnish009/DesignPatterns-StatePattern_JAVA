6a61d20473efc8f9a2e26c8c21bf22016073ee8d
Nishant Shah <nshah27@binghamton.edu>
2017-10-18 01:52:31 +0000

    HighRisk.java edited with Using the Logger file

--- airportSecurityState/src/airportSecurityState/airportStates/HighRisk.java
+++ airportSecurityState/src/airportSecurityState/airportStates/HighRisk.java
@@ -1,40 +1,37 @@
 package airportSecurityState.airportStates;
 
+import airportSecurityState.util.MyLogger;
+
 public class HighRisk implements AirportStateI {
 	
 	private SecurityFactors sf = null;
 
 	public HighRisk(SecurityFactors sfI) {
+		MyLogger.writeMessage("HighRisk constructor called", MyLogger.DebugLevel.CONSTRUCTOR);
 		sf = sfI;
 	}
 	
 	public void tightenOrLoosenSecurity() {
 		
-		System.out.println("currently in HIGH");
-		System.out.println(sf.getAvgTraffic());
-		System.out.println(sf.getAvgProhibited());
-		
 		if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) && (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
 			sf.setAsi(sf.getModerateRisk());
 			sf.setFlag(1);
 			
-			System.out.println("change to MOD");
+			MyLogger.writeMessage("State changed to ModerateRisk", MyLogger.DebugLevel.IN_RUN);
 		}
 		
 		else if((sf.getAvgTraffic() >= 0 && sf.getAvgTraffic() < 4) && (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
 			sf.setAsi(sf.getModerateRisk());
 			sf.setFlag(1);
 			
-			System.out.println("change to MOD");
+			MyLogger.writeMessage("State changed to ModerateRisk", MyLogger.DebugLevel.IN_RUN);
 		}
 		
 		else if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) && (sf.getAvgProhibited() >= 0 && sf.getAvgProhibited() < 1)) {
 			sf.setAsi(sf.getModerateRisk());
 			sf.setFlag(1);
 			
-			System.out.println("change to MOD");
+			MyLogger.writeMessage("State changed to ModerateRisk", MyLogger.DebugLevel.IN_RUN);
 		}
-		
-		
 	}
 }
