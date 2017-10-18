7ec30b76caeb2d5f7c98850b22bf8f2189b6b1f6
Nishant Shah <nshah27@binghamton.edu>
2017-10-18 01:53:40 +0000

    added Logger functionality to LowRisk.java

--- airportSecurityState/src/airportSecurityState/airportStates/LowRisk.java
+++ airportSecurityState/src/airportSecurityState/airportStates/LowRisk.java
@@ -1,22 +1,23 @@
 package airportSecurityState.airportStates;
 
+import airportSecurityState.util.MyLogger;
+
 public class LowRisk implements AirportStateI {
 	
 	private SecurityFactors sf = null;
 
 	public LowRisk(SecurityFactors sfI) {
+		MyLogger.writeMessage("LowRisk constructor called", MyLogger.DebugLevel.CONSTRUCTOR);
 		sf = sfI;
 	}
 	
 	public void tightenOrLoosenSecurity() {
 		
-		System.out.println("currently in LOW");
-		System.out.println(sf.getAvgTraffic());
-		System.out.println(sf.getAvgProhibited());
 		if((sf.getAvgTraffic() >= 4 && sf.getAvgTraffic() < 8) || (sf.getAvgProhibited() >= 1 && sf.getAvgProhibited() < 2)) {
 			sf.setAsi(sf.getModerateRisk());
 			sf.setFlag(1);
-			System.out.println("change to MOD");
+
+			MyLogger.writeMessage("State changed to ModerateRisk", MyLogger.DebugLevel.IN_RUN);
 		}
 	}
 }
